1dc4f30862ccce6b3e21beb56bf8cd071397aed5
BosonHiggs-qt <19520611@gm.uit.edu.vn>
2021-01-09 13:28:34 +0700

    FormRedux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapSinhVien from './components/FormSinhVien/BaiTapSinhVien';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapSinhVien/>
+    </div>
+  );
+}
+
+export default App;
